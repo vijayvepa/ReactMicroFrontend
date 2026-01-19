21724e17ace9c8e4f40014ea0e1d42900c2e40ce
Vijay Vepakomma <vijay.k.vepakomma@gmail.com>
2026-01-19 11:54:19 -0500

    fix

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
 	mode: 'development',
@@ -10,8 +9,4 @@ module.exports = {
 			template: './public/index.html'
 		})
 	]
-=======
-module.exports = {
-		mode: 'development'
->>>>>>> 853ae2b25ab7366e3049f50586db127f2c055f35
 }
